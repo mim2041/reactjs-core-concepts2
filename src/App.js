3beb8a9d9250b5344262053d431227f9424f6de1
mim2041 <suraiyamim338@gmail.com>
2023-03-23 23:19:26 +0600

    more js react core concepts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const products = [
+    {name: 'laptop', price: 153000},
+    {name: 'phone', price: 53000},
+    {name: 'watch', price: 3000},
+    {name: 'tablet', price: 5000},
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
     </div>
   );
 }
 
+function Counter(){
+  const [] = useState
+  return (
+    <div>
+      <h1>Count: </h1>
+      <button>Increase</button>
+    </div>
+  )
+}
+
 export default App;
+
+
+// {
+//   products.map(product => <Product name={product.name} price={product.price}></Product>)
+// }
+// {/* <Product name = "laptop" price="47000"></Product>
+// <Product name = "phone" price="75000"></Product>
+// <Product name = "watch" price="5000"></Product> */}
+
+// function Product(props){
+//   return (
+//     <div className='product'>
+//       <h3>Name: {props.name}</h3>
+//       <p>Price: </p>
+//     </div>
+//   )
+// }
