742361801e496004ea8799e84fe24a72653e0699
mim2041 <suraiyamim338@gmail.com>
2023-03-24 20:35:43 +0600

    useEffect to load and display users

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   return (
     <div className="App">
-      <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
     </div>
   );
+}
+
+function ExternalUsers(){
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  } ,[])
+    
+  return (
+    <div>
+      <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} phone={user.phone} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props){
+  return (
+    <div style={{border: '2px solid red', margin: '20px'}}>
+      <h3>Name: {props.name}</h3>
+      <p>Phone: {props.phone}</p>
+      <p>Email: {props.email}</p>
+    </div>
+  )
 }
 
 function Counter(){
@@ -27,6 +56,7 @@ function Counter(){
     </div>
   )
 }
+
 
 export default App;
 
